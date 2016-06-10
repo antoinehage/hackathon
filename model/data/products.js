f98b5aa09b16739cc3d62b3dae86b89a3150df31
Alain Gaeremynck <Alain.Gaeremynck@pj.ca>
2016-06-10 14:26:15 -0400

    adding products

--- model/data/products.js
+++ model/data/products.js
@@ -20,37 +20,45 @@ var products = [
         "price": 149.99,
         "images": [
             "http://www.thinkgeek.com/images/products/frontsquare/1db4_star_trek_phaser_remote_replica.jpg",
-            "",
-            "",
-            "",
-            ""
+            "http://www.thinkgeek.com/images/products/additional/carousel/1db4_star_trek_phaser_remote_replica_glow.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/1db4_star_trek_phaser_remote_replica_box_open.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/1db4_star_trek_phaser_remote_replica_box_closed.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/1db4_star_trek_phaser_remote_replica_scan.jpg"
         ]
     },
     {
         "id": "3",
-        "name": "",
-        "descrition": "",
-        "price":,
+        "name": "Talking Figures",
+        "descrition": "Pulp Fiction 13\" Talking Figures",
+        "price": 29.99,
         "images": [
-            ""
+            "http://www.thinkgeek.com/images/products/frontsquare/irjm_pulp_fiction_13in_talking_figs.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/irjm_pulp_fiction_13in_talking_figs_jimmie.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/irjm_pulp_fiction_13in_talking_figs_jules.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/irjm_pulp_fiction_13in_talking_figs_vincent.jpg"
         ]
     },
     {
         "id": "4",
-        "name": "",
-        "descrition": "",
-        "price":,
+        "name": "Lightsaber",
+        "descrition": "Star Wars Darth Vader Lightsaber Laser Pointer",
+        "price":14.99,
         "images": [
-            ""
+            "http://www.thinkgeek.com/images/products/frontsquare/c5e6_star_wars_laser_pointers.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/huih_sw_vader_lightsaber_laser_pointer_inhand.jpg"
         ]
     },
     {
         "id": "5",
-        "name": "",
-        "descrition": "",
-        "price":,
+        "name": "Tardis",
+        "descrition": "Doctor Who: 11th Doctor 1/6 Scale Collectibles",
+        "price":379.99,
         "images": [
-            ""
+            "http://www.thinkgeek.com/images/products/frontsquare/iqti_dw_11th_collectibles.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/iqti_dw_11th_collectibles_scale.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/iqti_dw_11th_collectibles_both.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/iqti_dw_11th_collectibles_fig2.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/iqti_dw_11th_collectibles_parts.jpg"
         ]
     }
 ]
