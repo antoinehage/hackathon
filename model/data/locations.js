9755112abb05c5185c2e031cf25d0943ec300eef
Alain Gaeremynck <Alain.Gaeremynck@pj.ca>
2016-06-10 10:38:44 -0400

    initial model

---
+++ model/data/locations.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+
+    "nordelec":{
+        "id":1,
+        "coor":{
+            "lat":1.1,
+            "long":1.1
+        },
+        "events":[]
+    }
+
+};
---
+++ model/data/themes.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+
+    "birthday": {
+        "id": 1,
+        "label": {
+            "fr": "anniversaire",
+            "en": "birthday"
+        }
+    },
+    "wedding": {
+        "id": 2,
+        "label": {
+            "fr": "mariage",
+            "en": "wedding"
+        }
+    },
+    "bachelor": {
+        "id": 3,
+        "label": {
+            "fr": "enterrement de vie de garçon",
+            "en": "bachelor party"
+        }
+    },
+    "wedding_anniversary": {
+        "id": 4,
+        "label": {
+            "fr": "anniversaire de mariage",
+            "en": "wedding annyversary"
+        }
+    },
+    "reunion": {
+        "id": 5,
+        "label": {
+            "fr": "réunion école secondaire",
+            "en": "high school reunion"
+        }
+    },    
+    "graduation": {
+        "id": 6,
+        "label": {
+            "fr": "graduation",
+            "en": "graduation"
+        }
+    }
+};
