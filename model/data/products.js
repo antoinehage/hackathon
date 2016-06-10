e01f2294bb0012dffb8ec3e5984a2506518f4b87
Alain Gaeremynck <Alain.Gaeremynck@pj.ca>
2016-06-10 14:20:23 -0400

    adding products

--- model/data/products.js
+++ model/data/products.js
@@ -1,3 +1,56 @@
-[
-    
+'use strict';
+
+var products = [
+    {
+        "id": "1",
+        "name": "BB-8",
+        "descrition": "App-Enabled Droid™ by Sphero",
+        "price": 149.99,
+        "images": [
+            "http://www.thinkgeek.com/images/products/frontsquare/ijgi_sphero_bb8.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/ijgi_sphero_bb8_add1.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/ijgi_sphero_bb8_add3.jpg",
+            "http://www.thinkgeek.com/images/products/additional/carousel/ijgi_sphero_bb8_add4.jpg"
+        ]
+    },
+    {
+        "id": "2",
+        "name": "Phaser",
+        "descrition": "Exclusive Star Trek Phaser Remote Replica",
+        "price": 149.99,
+        "images": [
+            "http://www.thinkgeek.com/images/products/frontsquare/1db4_star_trek_phaser_remote_replica.jpg",
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        "id": "3",
+        "name": "",
+        "descrition": "",
+        "price":,
+        "images": [
+            ""
+        ]
+    },
+    {
+        "id": "4",
+        "name": "",
+        "descrition": "",
+        "price":,
+        "images": [
+            ""
+        ]
+    },
+    {
+        "id": "5",
+        "name": "",
+        "descrition": "",
+        "price":,
+        "images": [
+            ""
+        ]
+    }
 ]
