4c122373ce4373a71414b036a2f47dc4134634e4
Alain Gaeremynck <Alain.Gaeremynck@pj.ca>
2016-06-10 10:51:13 -0400

    adding locations

--- model/data/locations.js
+++ model/data/locations.js
@@ -1,13 +1,72 @@
 'use strict';
-module.exports = {
-
-    "nordelec":{
-        "id":1,
-        "coor":{
-            "lat":1.1,
-            "long":1.1
+module.exports = [
+    {
+        "name": "Kazoom Cafe",
+        "address": {
+            "street": "160-2169, boul Marcel-Laurin",
+            "city": "Saint-Laurent",
+            "prov": "QC",
+            "pcode": "H4R 1K4"
         },
-        "events":[]
+        "geoCode": {
+            "latitude": "45.5192799",
+            "longitude": "-73.7026514"
+        },
+        "phones": [
+            {
+                "freecall": false,
+                "npa": "514",
+                "nxx": "331",
+                "num": "4386",
+                "dispNum": "514-331-4386",
+                "type": "primary"
+            }
+        ],
+        "photos": [
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_5b10a100ce65b72c82bc1dd17f3194df4180b40a_0_36699100_1394218980_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_5b10a100ce65b72c82bc1dd17f3194df4180b40a_0_36699100_1394218980_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_499581a860d67ae9a9018d4b402000a65ccec3af_0_67077000_1394218989_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_499581a860d67ae9a9018d4b402000a65ccec3af_0_67077000_1394218989_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_70f974c3bde1c43540f455638e63d743d37ebeb8_0_70947600_1394218993_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_70f974c3bde1c43540f455638e63d743d37ebeb8_0_70947600_1394218993_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_3434c4ff28b562513dffd23db010865f0cc0d51d_0_47727900_1394218997_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_3434c4ff28b562513dffd23db010865f0cc0d51d_0_47727900_1394218997_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_b6b798bddd4d12fa5010d3ff732a8f5e100230ca_0_09711900_1394219002_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_b6b798bddd4d12fa5010d3ff732a8f5e100230ca_0_09711900_1394219002_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_ea91ffdbd82553bf892de7c328b4c9a12114ea19_0_15119200_1394219007_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_ea91ffdbd82553bf892de7c328b4c9a12114ea19_0_15119200_1394219007_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_911256e1b69569e594e63529dad35dc7803f7f97_0_60096800_1394219012_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_911256e1b69569e594e63529dad35dc7803f7f97_0_60096800_1394219012_t.jpg",
+                "description": ""
+            },
+            {
+                "url": "http://cdn.media.yp.ca/6359118/pcc_photos_963c10671b750e3781ad1beaaf36b723b6e58e81_0_22610400_1394219017_r.jpg",
+                "thmbUrl": "http://cdn.media.yp.ca/6359118/pcc_photos_963c10671b750e3781ad1beaaf36b723b6e58e81_0_22610400_1394219017_t.jpg",
+                "description": ""
+            }
+        ],
+        themes:[1,2]
+    },
+    {
+        
     }
-
-};
+];
