43379108616ce3c3b90e7bd3f4c145188a5f431c
Alain Gaeremynck <Alain.Gaeremynck@pj.ca>
2016-06-10 10:40:47 -0400

    initial model

---
+++ model/schema/events.js
@@ -0,0 +1,77 @@
+"use strict";
+
+module.exports = {
+    theme: {
+        "type":"number",
+        "required":true,
+        "ref": "theme.id"
+    },
+    owner: {
+        type: "object",
+        "properties": {
+            "firstName": {
+                "required": true,
+                "type": "string"
+            },
+            "lastName": {
+                "required": true,
+                "type": "string"
+            },
+            "phone":{
+
+            },
+            "emails": {
+                "required": true,
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "address": {"type": "string", "format": "email", "required": true},
+                        "primary": {"type": "boolean", "required": true}
+                    }
+                }
+            }
+        }
+    },
+    "addresses": {
+        "type": "array",
+        "minItems": 1,
+        "uniqueItems": true,
+        "items": {
+            "type": "object",
+            "properties": {
+                "address1": {
+                    "type": "string",
+                    "required": true
+                }
+                ,
+                "address2": {
+                    "type": "string",
+                    "required": false
+                }
+                ,
+                "city": {
+                    "type": "string",
+                    "required": true
+                }
+                ,
+                "state": {
+                    "type": "string",
+                    "required": true,
+                    "pattern": "[A-Z]{2}"
+                }
+                ,
+                "zip": {
+                    "type": "string",
+                    "required": true
+                }
+                ,
+                "primary": {
+                    "type": "boolean",
+                    "required": true
+                }
+            }
+        }
+    }
+}
+;
